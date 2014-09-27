27ee18cf83be33bb6e7d4204f41ae060915467bf
Wildhoney <adam.timberlake@gmail.com>
2014-09-27 21:27:57 +0100

    First example

---
+++ components/ngRangeSlider.js
@@ -0,0 +1,12 @@
+(function($angular) {
+
+    "use strict";
+
+    // No, no! The adventures first; explanations take such a dreadful time.
+    $angular.module('ngRangeSlider', []).directive('rangeSlider', function ngRangeSlider() {
+
+
+
+    });
+
+})(window.angular);
