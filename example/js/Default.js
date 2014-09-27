7b2a5352c6b25e930df2b75eb24fec8c75b9c136
Wildhoney <adam.timberlake@gmail.com>
2014-09-27 21:27:48 +0100

    Added initial example

---
+++ example/js/Default.js
@@ -0,0 +1,14 @@
+(function($angular) {
+
+    // Off we go!
+    $angular.module('rangeApp', ['ngRangeSlider']).controller('IndexController', function IndexController($scope) {
+
+        /**
+         * @property range
+         * @type {{from: number, to: number}}
+         */
+        $scope.range = { from: 0, to: 10 };
+
+    });
+
+})(window.angular);
